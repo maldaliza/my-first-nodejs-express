8e76ca26ce90fce556fd131fba5f03d5ee031e35
jaekook <jaekook4849@gmail.com>
2020-10-27 10:51:59 +0900

    version 1.0.2

--- server.js
+++ server.js
@@ -25,7 +25,10 @@ app.get('/page/:pageId', function(request, response) {
         fs.readFile(`./data/${request.params.pageId}`, 'utf-8', function(err, description) {
             const title = request.params.pageId;
             const list = template.list(fileList);
-            const control = '<a href="/create">create</a>';
+            const control = `
+                <a href="/create">create</a>
+                <a href="/update/${request.params.pageId}">update</a>
+            `;
             const body = `
                 <h2>${title}</h2>
                 <p>${description}</p>
@@ -43,6 +46,7 @@ app.get('/create', function(request, response) {
         const list = template.list(fileList);
         const control = '';
         const body = `
+            <h2>${title}</h2>
             <form action="/create_process" method="POST">
                 <p><input type="text" name="title" placeholder="title"></p>
                 <p><textarea name="description" placeholder="description" cols="30" rows="10"></textarea></p>
@@ -66,11 +70,55 @@ app.post('/create_process', function(request, response) {
         const description = post.description;
 
         fs.writeFile(`./data/${title}`, description, 'utf-8', function(error) {
+            if(error) throw error;
             /*
             response.writeHead(302, {Location: `/page/${title}`});
             response.end();
             */
             response.redirect(`/page/${title}`);
+        });
+    });
+});
+
+app.get('/update/:pageId', function(request, response) {
+    fs.readdir('./data', function(error, fileList) {
+        fs.readFile(`./data/${request.params.pageId}`, 'utf-8', function(err, description) {
+            const title = request.params.pageId;
+            const list = template.list(fileList);
+            const control = '';
+            const body = `
+                <form action="/update_process" method="POST">
+                    <input type="hidden" name="id" value="${title}">
+                    <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+                    <p><textarea name="description" placeholder="description" cols="30" rows="10">${description}</textarea></p>
+                    <p><input type="submit"></p>
+                </form>
+            `;
+            const html = template.HTML(title, list, control, body);
+
+            response.send(html);
+        });
+    });
+});
+
+app.post('/update_process', function(request, response) {
+    let body = '';
+    request.on('data', function(data) {
+        body = body + data;
+    });
+    request.on('end', function() {
+        const post = qs.parse(body);
+        const id = post.id;
+        const title = post.title;
+        const description = post.description;
+
+        fs.rename(`./date/${id}`, `./data/${title}`, function(error) {
+            // if(error) throw error;
+            // 에러처리에 대해 공부하기
+            fs.writeFile(`./data/${title}`, description, 'utf-8', function(err) {
+                if(err) throw err;
+                response.redirect(`/page/${title}`);
+            });
         });
     });
 });
